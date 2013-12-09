879286dd76bc83ffb48d367543523317b73679e0
Håvard Sørbø <havard@hsorbo.no>
2013-12-09 23:35:42 +0100

    Added event-loop, event-enablers

--- comoyo-sendsms.py
+++ comoyo-sendsms.py
@@ -11,6 +11,8 @@ import json
 import uuid
 import os
 import argparse
+import thread
+import threading
 
 SESSION_FILE = os.path.expanduser("~/.sendsms.json")
 
@@ -26,22 +28,66 @@ CLIENT_INFO =  {
 class ComoyoSMS():
 	def __init__(self, sslSocket):
 		self._sslSocket = sslSocket
+		self._subscribers = []
+		def debug(x): 
+			print "RECIEVE:"
+			print(json.dumps(x, indent=1))
+		self._subscribers.append(debug)
+		thread.start_new_thread(self._eventLoop, () )
 
-	def _send_command(self, command):
+	def _eventLoop(self):
+		read = ""
+		while True:
+			data = self._sslSocket.read(1024)
+			#print data
+			if not data: 
+				print("disconnect")
+				for subscriber in self._subscribers:
+						subscriber(None)
+				break
+			
+			read += data;
+
+			if read[-1] == "\x00":
+				for chunk in read.split("\x00"):
+					#use filter
+					if chunk == "": continue
+					for subscriber in self._subscribers:
+						subscriber(json.loads(chunk))
+				read = ""
+
+	def _send_command(self, command, response_format = None):
+		evt = threading.Event()
+		chunks = []
+		r = {}
+		def f(response):
+			if response_format == None or response == None:
+				r["response"] = None
+				evt.set()
+			elif response.has_key(response_format):
+				r["response"] = response[response_format]
+				evt.set()
+			
+	
+		self._subscribers.append(f)
+		print "SEND:"
+		print json.dumps(command, indent=1)
 		self._sslSocket.write(json.dumps(command) + "\x00")
-		response = self._sslSocket.read()
-		return json.loads(response.replace("\x00",""))
+		#sem.acquire()
+		evt.wait(3)
+		self._subscribers.remove(f)
+		response = r["response"]
+		return response
 
 	def activate(self):	
 		activate_command = {"com.telenor.sw.adaptee.th.ClientActiveCommand" : { "clientInformation" : CLIENT_INFO }}
-		print self._send_command(activate_command)
+		response = self._send_command(activate_command, "com.telenor.sw.footee.common.th.ClientActiveResponse")
 
 	def authenticate(self, auth_info):
 		#destructive, fix
 		auth_info["commandVersion"] = 0
 		auth_command = {"com.telenor.sw.adaptee.th.AuthenticateSessionCommand" : { "authenticateSessionInformation" : auth_info }}
-		print self._send_command(auth_command)
-		print self._sslSocket.read()
+		response = self._send_command(auth_command, "com.telenor.sw.footee.common.th.AuthenticateSessionResponse")
 
 	def send_sms(self, number, message):
 		message = {
@@ -51,7 +97,7 @@ class ComoyoSMS():
 			"token": str(uuid.uuid1())
 		}
 		send_sms_command = {"com.telenor.sw.adaptee.th.SendSmsCommand" : { "smsMessage" : message }}
-		print self._send_command(send_sms_command)
+		response = self._send_command(send_sms_command)
 
 	def register(self):
 		register_command = {"com.telenor.sw.adaptee.th.ClientRegistrationCommand" : { "clientInformation" : CLIENT_INFO}}
@@ -67,6 +113,30 @@ class ComoyoSMS():
 		registration_response["clientId"] = clientId
 		return registration_response
 
+	def get_conversations(self):
+		generationRange = {"generationRangeStart" : 1, "generationRangeEnd": 999999999999999999999999999999999999 }
+		command = {
+			"com.telenor.sw.adaptee.th.ConversationUpdateRequestCommand":
+			{"generationRange": generationRange}
+		}
+		return self._send_command(command, "com.telenor.sw.adaptee.th.ConversationUpdateResponse")
+	
+	def enable_smsplus(self):
+		"""Dunno
+		"""
+		command = {"com.telenor.sw.adaptee.th.ServiceRequestCommand":{"serviceId":"smsplus"}}
+		self._send_command(command, "com.telenor.sw.footee.common.th.ServiceResponse")
+
+	def enable_subscription(self, subscribeToContactUpdates = True, subscribeToConversationUpdates = True):
+		command = {
+			"com.telenor.sw.adaptee.th.SubscriptionCommand":{
+				"subscriptionInformation":{
+					"subscribeToContactUpdates": subscribeToContactUpdates,
+					"subscribeToConversationUpdates": subscribeToConversationUpdates
+				}
+			}
+		}
+		self._send_command(command)	
 
 def load_settings():
 	f = open(SESSION_FILE, 'r')
